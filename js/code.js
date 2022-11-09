05c202fd0ac27fd84222cc5fc69be3509906f48f
Brogy-alt <brogangys2@gmail.com>
2022-11-09 09:40:27 +0200

    yeah yeah

---
+++ js/code.js
@@ -0,0 +1,24 @@
+//First approach : make use of function
+
+// EVA
+
+// let firstName = prompt("please enter your name:");
+// let surname = prompt("Please enter your surname:");
+// let salary = prompt("Please enter your salary: ",0);
+// document.write(`
+//     Name: ${firstName}<br>
+//     Surname: ${surname}<br>
+//     Salary: R${eval(`${salary}*2`)}`);
+
+
+// function addition () {
+//     return 9 + 2;
+// }
+
+// document.write(`Sum: ${addition()}`);
+
+let addition = ()=>{
+    document.write(997+ 2);
+}
+
+addition();
